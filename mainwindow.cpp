468e90d5db9012fe53fe3e05e6e75431fae36026
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-03-05 20:01:54 +0300

    Update mainwindow.cpp

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,6 +7,7 @@ MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
 }
 
@@ -18,9 +19,11 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-
-
     ui->engine->load(QUrl("https://m.vk.com/innopolis"));
     ui->engine->show();
+    ui->enginetwo->load(QUrl("http://welcome.innopolis.ru/vacancies/"));
+        ui->enginetwo->show();
+
+
 
 }
