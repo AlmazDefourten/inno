5eee4b21d6f9ac896802bd6e82b3f3288d81602c
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-03-08 22:01:52 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include "anotherwindow.h"
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +17,13 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
+
+
 private:
     Ui::MainWindow *ui;
+    AnotherWindow *sWindow;
 };
 #endif // MAINWINDOW_H
