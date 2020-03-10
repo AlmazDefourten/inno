10e590ecb2575149fdd6518adeca02479a819883
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-03-10 21:53:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,21 @@
 #include "mainwindow.h"
-
+#include "univers.h"
+#include "ui_Univers.h"
+#include <QtWebEngineWidgets>
+#include <QtWebEngine/qtwebengineglobal.h>
 #include <QApplication>
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
 
+
+#include <QQmlApplicationEngine>
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
+    QApplication a(argc, argv);  
+        MainWindow w;
+        w.show();
+    
+        return a.exec();
+    }
+    
+    
