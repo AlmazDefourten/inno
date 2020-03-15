c7110619c052a5d7a623aadb2586f2d42392c42e
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-03-15 22:35:40 +0300

    Create university.h

---
+++ university/university.h
@@ -0,0 +1,26 @@
+#ifndef UNIVERSITY_H
+#define UNIVERSITY_H
+
+#include <QDialog>
+
+namespace Ui {
+class university;
+}
+
+class university : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit university(QWidget *parent = nullptr);
+    void unEngineShow();
+    ~university();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::university *ui;
+};
+
+#endif // UNIVERSITY_H
