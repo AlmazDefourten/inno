d78f1bc4f247edbefacf75531aab7aaeb5ae0f5b
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-03-15 22:28:06 +0300

    Create school.h

---
+++ school/school.h
@@ -0,0 +1,27 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+
+#include <QDialog>
+#include "mainwindow.h"
+namespace Ui {
+class school;
+}
+
+class school : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit school(QWidget *parent = nullptr);
+    void scEngineShow();
+    ~school();
+
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    Ui::school *ui;
+
+};
+
+#endif // SCHOOL_H
