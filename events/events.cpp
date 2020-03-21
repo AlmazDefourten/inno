fb1d958ca8de3869412a13b17553b3d10e496d16
D410 <58397287+AlmazDefourten@users.noreply.github.com>
2020-03-21 13:17:14 +0300

    Create events.cpp

---
+++ events/events.cpp
@@ -0,0 +1,20 @@
+#include "events.h"
+#include "ui_events.h"
+#include "mainwindow.h"
+
+events::events(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::events)
+{
+    ui->setupUi(this);
+}
+
+events::~events()
+{
+    delete ui;
+}
+
+void events::on_pushButton_clicked()
+{
+    ui->evEngine->load(QUrl("http://innopolis.ru/media/events/"));
+}
